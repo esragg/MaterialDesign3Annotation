ce0f30111976615b0f222f922ccb5a140036fce3
Esra GARCIA GORMAN <esragarcia@hotmail.com>
2023-07-03 13:03:42 +0200

    Material Design 3 Annotation Ekleme Hata Duzeltme

---
+++ app/src/main/java/com/example/materialdesign3annotationekleme/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.materialdesign3annotationekleme
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.materialdesign3annotationekleme.ui.theme.MaterialDesign3AnnotationEklemeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialDesign3AnnotationEklemeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Sayfa()
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Sayfa() {
+    Scaffold(
+        topBar = {
+
+        },
+        content = {
+
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MaterialDesign3AnnotationEklemeTheme {
+        Sayfa()
+    }
+}
